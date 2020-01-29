58593b0b7b4394955a2147912ef4207dbd403a57
akakou <akakou571@gmail.com>
2020-01-30 02:22:32 +0900

    [fix] Timeing that jump to root directory

--- utils/tools.py
+++ utils/tools.py
@@ -1,6 +1,8 @@
 import os
+import utils.common import get_file_path
 
 
 def launch():
     '''Launch docker-compose.'''
-    os.system('docker-compose up')
+    path = get_file_path('')
+    os.system(f'cd {path} && docker-compose up')
