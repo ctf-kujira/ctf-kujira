c5ec79ccc1a60462e117475f5b96f4b07ee04911
akakou <akakou571@gmail.com>
2020-01-31 11:49:18 +0900

    [add] docker-compose run

--- utils/compose.py
+++ utils/compose.py
@@ -3,6 +3,12 @@ from utils.common import get_file_path
 
 
 def up():
-    '''Launch docker-compose.'''
+    '''$ docker-compose up'''
     path = get_file_path('')
     os.system(f'cd {path} && docker-compose up')
+
+
+def run(args):
+    '''$ docker-compose run'''
+    path = get_file_path('')
+    os.system(f'cd {path} && docker-compose run {args}')
